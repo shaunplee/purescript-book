11c4f3a10595a44f93c68ff3b63d682240a6e7d2
Shaun Lee <shaun@curlyfri.es>
2021-02-16 21:15:09 -0800

    Reading

--- exercises/chapter12/src/Example/Random.purs
+++ exercises/chapter12/src/Example/Random.purs
@@ -2,13 +2,12 @@ module Example.Random where
 
 import Prelude
 
-import Effect (Effect)
-import Effect.Random (random)
 import Data.Array ((..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (strokePath, fillPath, arc, setStrokeStyle,
-                        setFillStyle, getContext2D, getCanvasElementById)
+import Effect (Effect)
+import Effect.Random (random)
+import Graphics.Canvas (Context2D(..), arc, fillPath, getCanvasElementById, getContext2D, restore, save, setFillStyle, setStrokeStyle, strokePath)
 import Math as Math
 import Partial.Unsafe (unsafePartial)
 
@@ -20,11 +19,13 @@ main = void $ unsafePartial do
   setFillStyle ctx "#F00"
   setStrokeStyle ctx "#000"
 
-  for_ (1 .. 100) \_ -> do
+  for_ (1 .. 100) \n -> do
     x <- random
     y <- random
     r <- random
-
+    if n == 80 then
+      restore ctx
+      else pure unit
     let path = arc ctx
          { x     : x * 600.0
          , y     : y * 600.0
@@ -33,5 +34,9 @@ main = void $ unsafePartial do
          , end   : Math.tau
          }
 
-    fillPath ctx path
-    strokePath ctx path
+    fillAndStroke ctx path
+
+fillAndStroke :: forall a. Context2D -> Effect a -> Effect a
+fillAndStroke ctx path = do
+  _ <- fillPath ctx path
+  strokePath ctx path
--- exercises/chapter12/src/Example/Shapes.purs
+++ exercises/chapter12/src/Example/Shapes.purs
@@ -2,53 +2,102 @@ module Example.Shapes where
 
 import Prelude
 
-import Effect (Effect)
+import Data.Array ((..), head)
+import Data.Enum (enumFromThenTo)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (closePath, lineTo, moveTo, fillPath,
-                        setFillStyle, arc, rect, getContext2D,
-                        getCanvasElementById)
+import Data.Traversable (traverse)
+import Effect (Effect)
+import Graphics.Canvas (Context2D(..), arc, closePath, fillPath, getCanvasElementById, getContext2D, lineTo, moveTo, rect, setFillStyle, setLineWidth, setStrokeStyle, strokePath)
 import Math as Math
 import Partial.Unsafe (unsafePartial)
 
-translate
-  :: forall r
-   . Number
-  -> Number
-  -> { x :: Number, y :: Number | r }
-  -> { x :: Number, y :: Number | r }
-translate dx dy shape = shape
-  { x = shape.x + dx
-  , y = shape.y + dy
-  }
+translate ::
+  forall r.
+  Number ->
+  Number ->
+  { x :: Number, y :: Number | r } ->
+  { x :: Number, y :: Number | r }
+translate dx dy shape =
+  shape
+    { x = shape.x + dx
+    , y = shape.y + dy
+    }
 
 main :: Effect Unit
-main = void $ unsafePartial do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
+main =
+  void
+    $ unsafePartial do
+        Just canvas <- getCanvasElementById "canvas"
+        ctx <- getContext2D canvas
+        setFillStyle ctx "#00F"
+        fillPath ctx
+          $ do
+              rect ctx
+                $ translate (-200.0) (-200.0)
+                    { x: 250.0
+                    , y: 250.0
+                    , width: 100.0
+                    , height: 100.0
+                    }
+              rect ctx
+                $ translate (-50.0) (-200.0)
+                    { x: 250.0
+                    , y: 250.0
+                    , width: 100.0
+                    , height: 100.0
+                    }
+        setFillStyle ctx "#0F0"
+        setStrokeStyle ctx "black"
+        strokePath ctx $ arc ctx
+          $ translate 200.0 200.0
+              { x: 300.0
+              , y: 300.0
+              , radius: 50.0
+              , start: 0.0
+              , end: Math.tau * 2.0 / 3.0
+              }
+        fillPath ctx
+          $ do
+              arc ctx
+                $ { x: 200.0
+                  , y: 200.0
+                  , radius: 150.0
+                  , start: Math.tau * 1.0 / 12.0
+                  , end: Math.tau * 3.0 / 12.0
+                  }
+              lineTo ctx 200.0 200.0
+              closePath ctx
+        setFillStyle ctx "#F00"
+        setLineWidth ctx 2.0
+        strokePath ctx
+          $ do
+              moveTo ctx 300.0 260.0
+              lineTo ctx 260.0 340.0
+              lineTo ctx 340.0 340.0
+              closePath ctx
+        fillPath ctx
+          $ do
+              moveTo ctx 300.0 260.0
+              lineTo ctx 260.0 340.0
+              lineTo ctx 340.0 340.0
+              closePath ctx
+        renderPath ctx
+          (map (translate 100.0 500.0)
+          (map f (map ((_ / 10.0) <<< toNumber) ((-100) .. 100))))
+    where
+      f :: Number -> Point
+      f k = {x: 10.0 * k, y: -10.0 * k * k}
 
-  setFillStyle ctx "#00F"
+type Point
+  = { x :: Number, y :: Number }
 
-  fillPath ctx $ rect ctx $ translate (-200.0) (-200.0)
-    { x: 250.0
-    , y: 250.0
-    , width: 100.0
-    , height: 100.0
-    }
-
-  setFillStyle ctx "#0F0"
-
-  fillPath ctx $ arc ctx $ translate 200.0 200.0
-    { x: 300.0
-    , y: 300.0
-    , radius: 50.0
-    , start: 0.0
-    , end: Math.tau * 2.0 / 3.0
-    }
-
-  setFillStyle ctx "#F00"
-
-  fillPath ctx $ do
-    moveTo ctx 300.0 260.0
-    lineTo ctx 260.0 340.0
-    lineTo ctx 340.0 340.0
-    closePath ctx
+renderPath :: Context2D -> Array Point -> Effect Unit
+renderPath ctx ps = do
+  setStrokeStyle ctx "black"
+  case head ps of
+    Nothing -> pure unit
+    Just h -> strokePath ctx $ do
+      moveTo ctx h.x h.y
+      _ <- traverse (\p -> lineTo ctx p.x p.y) ps
+      pure unit
